9cb315455ae87028dfa42704c4bdad1250512dc1
Rebekah Dominguez <ridominguez2@buffs.wtamu.edu>
2024-02-01 21:46:28 -0600

    progress

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,1 +1,18 @@
-print("hello world")
+def whole_number_to_roman_numeral(num):
+    if num < 1 or num > 5:
+        print("Does not exist")
+
+    small_numbers = {
+        1: 'i',
+        2: 'ii',
+        3: 'ii'
+    }
+
+    return small_numbers[num]
+
+
+converter = int(input("Enter whole number: "))
+
+result = whole_number_to_roman_numeral(converter)
+
+print(f"Whole Number: {converter}, Roman Numeral: {result}")
